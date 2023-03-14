fe3ef8aa5276b8a0a4dca6ab876cb6dd9ee437b0
胡鑫 <1219654535@qq.com>
2023-03-14 15:13:57 +0800

    chore: remove console

--- packages/unocss-preset-theming/src/index.ts
+++ packages/unocss-preset-theming/src/index.ts
@@ -1,17 +1,7 @@
-/*
- * @Author: 胡鑫 1219654535@qq.com
- * @Date: 2023-03-09 16:18:36
- * @LastEditors: Aaron.ux
- * @LastEditTime: 2023-03-11 18:07:58
- * @FilePath: \vue-turbo-starter\packages\unocss-preset-theming\src\index.ts
- * @Description:
- *
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
- */
 import type { Preset, Rule } from '@unocss/core'
 import type { Theme } from '@unocss/preset-mini'
 import { set } from 'lodash-es'
-import { isHex, getRgbValue } from '@jiaminghi/color'
+import { getRgbValue, isHex } from '@jiaminghi/color'
 
 export type ThemingOptions = Array<{
   name: string
@@ -19,9 +9,9 @@ export type ThemingOptions = Array<{
   selectors?: string[]
 }>
 
-type varsTheme = { style: Theme | string, varName: string, keyName: string, callBack: (varsTheme: varsTheme) => void }
+interface varsTheme { style: Theme | string; varName: string; keyName: string; callBack: (varsTheme: varsTheme) => void }
 
-export function presetTheming (_options: ThemingOptions = []): Preset<Theme> {
+export function presetTheming(_options: ThemingOptions = []): Preset<Theme> {
   // 变量前缀
   const prefix = '--un-theme'
   // 校验是否是变量
@@ -37,10 +27,11 @@ export function presetTheming (_options: ThemingOptions = []): Preset<Theme> {
    * @param {object} theme
    * @return {*}
    */
-  function convertColorVars (theme: varsTheme) {
+  function convertColorVars(theme: varsTheme) {
     if (typeof theme.style === 'string') {
       theme.callBack(theme)
-    } else {
+    }
+    else {
       Object.entries(theme.style).forEach(([k, v]) => {
         convertColorVars({ ...theme, style: v, varName: `${theme.varName}-${k}`, keyName: theme.keyName ? `${theme.keyName}.${k}` : k })
       })
@@ -57,7 +48,7 @@ export function presetTheming (_options: ThemingOptions = []): Preset<Theme> {
       style: t.theme,
       varName: prefix,
       keyName: '',
-      callBack (vars) {
+      callBack(vars) {
         const isColor = isHex(vars.style as string)
         // if (usedTheme.includes(vars.varName)) {
         rule[vars.varName] = isColor ? getRgbValue(vars.style as string).join(',') : vars.style
@@ -71,8 +62,6 @@ export function presetTheming (_options: ThemingOptions = []): Preset<Theme> {
       generateCSS.push(`${selector}{ ${Object.keys(rule).map(k => `${k}:${rule[k]};`).join('')} }`)
     })
 
-    console.log('rule', rule)
-
     return [
       t.name,
       rule,
@@ -83,8 +72,7 @@ export function presetTheming (_options: ThemingOptions = []): Preset<Theme> {
     name: 'unocss-preset-theming',
     // enforce: 'post',
     // layers: { theming: -5 },
-    extendTheme (_originalTheme) {
-      console.log(_originalTheme)
+    extendTheme(_originalTheme) {
       // mergeDeep(originalTheme, baseTheme)
     },
     rules,
@@ -97,13 +85,12 @@ export function presetTheming (_options: ThemingOptions = []): Preset<Theme> {
       },
     ],
     theme: baseTheme,
-    postprocess (util) {
+    postprocess(util) {
       util.entries.forEach(([, val]) => {
         if (typeof val === 'string') {
           const varName = val.match(varsRE)?.[1]
-          if (varName) {
+          if (varName)
             usedTheme.push(varName)
-          }
         }
       })
     },
