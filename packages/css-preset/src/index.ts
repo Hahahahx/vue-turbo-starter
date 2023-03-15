bbe6dbcbaf54c296ef006ea86411b2a0228a3ce7
胡鑫 <1219654535@qq.com>
2023-03-15 17:46:42 +0800

    chore: remove variants,shortcuts

--- packages/css-preset/src/index.ts
+++ packages/css-preset/src/index.ts
@@ -1,38 +1,39 @@
-import type { UnocssNuxtOptions } from '@unocss/nuxt'
-import type { PresetWindOptions } from '@unocss/preset-wind'
-import { presetAttributify, presetIcons, presetMini, presetUno, presetWind, transformerDirectives, transformerVariantGroup } from 'unocss'
+import { presetAttributify, presetIcons, presetTypography, presetUno, presetWebFonts, transformerDirectives, transformerVariantGroup } from 'unocss'
 import type { UserConfig } from '@unocss/core'
 import { rules } from './rules'
 import { presetTheme } from './theme'
 import { shortcuts } from './shortcuts'
 
-interface CustomOptions {
-  /**
-   * Custom options for PresetWind
-   */
-  customPresetWindOptions?: PresetWindOptions
-}
-interface CustomUserConfig extends UserConfig, CustomOptions {}
-interface CustomNuxtConfig extends UnocssNuxtOptions, CustomOptions {}
-
 /**
  * Extends unocss/vite Plugin Options Config
  */
-export const extendUnocssOptions = ({ customPresetWindOptions, ...options }: CustomUserConfig = {}): UserConfig => {
+export const extendUnocssOptions = ({ ...options }: UserConfig = {}): UserConfig => {
   return {
     ...options,
     rules,
     shortcuts,
     presets: [
-      presetUno(),
-      presetMini(),
+      presetUno({
+        attributifyPseudo: true,
+      }),
+      // presetMini(),
       presetAttributify(),
       presetIcons({
+        extraProperties: {
+          'color': 'inherit',
+          // Avoid crushing of icons in crowded situations
+          'min-width': '1.2em',
+        },
         scale: 1.2,
         cdn: 'https://esm.sh/',
       }),
-      presetWind({
-        ...(customPresetWindOptions || {}),
+      // presetWind(),
+      presetTypography(),
+      presetWebFonts({
+        provider: 'none',
+        fonts: {
+          script: 'Homemade Apple',
+        },
       }),
       presetTheme,
       ...(options.presets || []),
@@ -42,15 +43,7 @@ export const extendUnocssOptions = ({ customPresetWindOptions, ...options }: Cus
       transformerVariantGroup(),
       ...(options.transformers || []),
     ],
-  }
-}
-
-/**
- * Extends unocss/nuxt Plugin Options Config
- */
-export function extendUnocssNuxtOptions(options: CustomNuxtConfig = {}): UnocssNuxtOptions {
-  return {
-    preflight: true,
-    ...extendUnocssOptions(options),
+    variants: [
+    ],
   }
 }
--- packages/css-preset/src/shortcuts.ts
+++ packages/css-preset/src/shortcuts.ts
@@ -1,6 +1,4 @@
 import type { UserShortcuts } from '@unocss/core'
 
 export const shortcuts: UserShortcuts<{}> = {
-  'group-test': 'bg-black ',
-  'group': '[&>.group-item]:rounded-none [&>.group-item]:border-r-0 first:[&>.group-item]:rounded-l last:[&>.group-item]:rounded-r',
 }
